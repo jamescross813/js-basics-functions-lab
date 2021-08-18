beb013cf28a7c0533086d06bab853940b8a6961c
jamescross <crossjames813@gmail.com>
2021-08-17 20:21:35 -0500

    commit tree 9b57bb5b022a5f807fd7e01aaa4c6d449fb06805

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+ let hq = 42
+ if (distance < 42){
+    return hq - distance
+ }else{
+     return distance - hq
+ }
+}
+
+function distanceFromHqInFeet(distance){    
+    return distanceFromHqInBlocks(distance)*264
+}
+
+function distanceTravelledInFeet(startBlock, endBlock){
+    let distanceTravelled 
+    if (startBlock>endBlock){
+        distanceTravelled = startBlock - endBlock
+    }else{
+        distanceTravelled = endBlock - startBlock
+    }
+    return distanceTravelled*264
+}
+
+function calculatesFarePrice(start, destination){
+    let distanceTravelled = distanceTravelledInFeet(start, destination)
+    if(distanceTravelled < 400){
+        return 0
+    }else if(distanceTravelled > 400 && distanceTravelled < 2000){
+        return (distanceTravelled - 400)*0.02
+    }else if (distanceTravelled >2000 && distanceTravelled < 2500){
+        return 25
+    }else{
+        return 'cannot travel that far'
+    }
+}
